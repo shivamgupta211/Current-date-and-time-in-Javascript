adb8c68335a47a2c0f30ad462c756f985173b2f5
Shivam Gupta <shivamgupta211@users.noreply.github.com>
2016-12-06 15:18:51 +0530

    Update functions.js

--- functions.js
+++ functions.js
@@ -8,8 +8,8 @@ function __getCurrentDateTime(format){
 	date['yy']=dt.getFullYear().toString().slice(-2);
 	date['h']=(dt.getHours()>12?dt.getHours()-12:dt.getHours());
 	date['hh']=dt.getHours();
-	date['mi']=dt.getMinutes();
-	date['mimi']=dt.getMinutes()<10?('0'+dt.getMinutes()):dt.getMinutes();
+	date['i']=dt.getMinutes();
+	date['ii']=dt.getMinutes()<10?('0'+dt.getMinutes()):dt.getMinutes();
 	date['s']=dt.getSeconds();
 	date['ss']=dt.getSeconds()<10?('0'+dt.getSeconds()):dt.getSeconds();
 	date['sss']=dt.getMilliseconds();
@@ -19,9 +19,23 @@ function __getCurrentDateTime(format){
 	x=x.indexOf('mm')!=-1?x.replace(/(mm)/i,date['mm']):x.replace(/(m)/i,date['m']);
 	x=x.indexOf('yyyy')!=-1?x.replace(/(yyyy)/i,date['yyyy']):x.replace(/(yy)/i,date['yy']);
 	x=x.indexOf('hh')!=-1?x.replace(/(hh)/i,date['hh']):x.replace(/(h)/i,date['h']);
-	x=x.indexOf('mimi')!=-1?x.replace(/(mimi)/i,date['mimi']):x.replace(/(mi)/i,date['mi']);
+	x=x.indexOf('ii')!=-1?x.replace(/(ii)/i,date['ii']):x.replace(/(i)/i,date['i']);
 	if(x.indexOf('sss')!=-1){	x=x.replace(/(sss)/i,date['sss']);	}
 	x=x.indexOf('ss')!=-1?x.replace(/(ss)/i,date['ss']):x.replace(/(s)/i,date['s']);
 	if(x.indexOf('ampm')!=-1){	x=x.replace(/(ampm)/i,date['ampm']);	}
 	return x;
+}
+
+
+function __getBackDateTime(f,format){
+	f=f.toLowerCase().split(' ');
+	var dt=new Date(),arr={};
+	for(var i in f){ 	arr[f[i].slice(0,1)]=f[i].substr(1);	}
+	arr['y']?dt.setFullYear(dt.getFullYear()+parseInt(arr['y'])):null;
+	arr['m']?dt.setMonth(dt.getMonth()+parseInt(arr['m'])):null;
+	arr['d']?dt.setDate(dt.getDate()+parseInt(arr['d'])):null;
+	arr['h']?dt.setHours(dt.getHours()+parseInt(arr['h'])):null;
+	arr['i']?dt.setMinutes(dt.getMinutes()+parseInt(arr['i'])):null;
+	arr['s']?dt.setSeconds(dt.getSeconds()+parseInt(arr['s'])):null;
+	return __getCurrentDateTime(format,dt);
 }
