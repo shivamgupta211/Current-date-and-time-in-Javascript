38c8f6792c13a22439cddaea982c90bcebc2e9a7
Shivam Gupta <shivamgupta211@users.noreply.github.com>
2016-12-06 20:32:30 +0530

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,4 +1,4 @@
-function __getCurrentDateTime(format='yyyy-mm-dd hh:mimi:ss',dt=new Date()){
+function __getCurrentDateTime(format='yyyy-mm-dd hh:ii:ss',dt=new Date()){
 	var x,date=[];
 	date['d']=dt.getDate();
 	date['dd']=dt.getDate()>10?dt.getDate():'0'+dt.getDate();
@@ -15,14 +15,14 @@ function __getCurrentDateTime(format='yyyy-mm-dd hh:mimi:ss',dt=new Date()){
 	date['sss']=dt.getMilliseconds();
 	date['ampm']=(dt.getHours()>=12?'PM':'AM');
 	x=format.toLowerCase();
-	x=x.indexOf('dd')!=-1?x.replace(/(dd)/i,date['dd']):x.replace(/(d)/i,date['d']);
-	x=x.indexOf('mm')!=-1?x.replace(/(mm)/i,date['mm']):x.replace(/(m)/i,date['m']);
-	x=x.indexOf('yyyy')!=-1?x.replace(/(yyyy)/i,date['yyyy']):x.replace(/(yy)/i,date['yy']);
-	x=x.indexOf('hh')!=-1?x.replace(/(hh)/i,date['hh']):x.replace(/(h)/i,date['h']);
-	x=x.indexOf('ii')!=-1?x.replace(/(ii)/i,date['ii']):x.replace(/(i)/i,date['i']);
-	if(x.indexOf('sss')!=-1){	x=x.replace(/(sss)/i,date['sss']);	}
-	x=x.indexOf('ss')!=-1?x.replace(/(ss)/i,date['ss']):x.replace(/(s)/i,date['s']);
-	if(x.indexOf('ampm')!=-1){	x=x.replace(/(ampm)/i,date['ampm']);	}
+	if(x.indexOf('ampm')!=-1){	x=x.replace(/(ampm)/,date['ampm']);	}
+	x=x.indexOf('dd')!=-1?x.replace(/(dd)/,date['dd']):x.replace(/(d)/i,date['d']);
+	x=x.indexOf('mm')!=-1?x.replace(/(mm)/,date['mm']):x.replace(/(m)/,date['m']);
+	x=x.indexOf('yyyy')!=-1?x.replace(/(yyyy)/,date['yyyy']):x.replace(/(yy)/,date['yy']);
+	x=x.indexOf('hh')!=-1?x.replace(/(hh)/,date['hh']):x.replace(/(h)/,date['h']);
+	x=x.indexOf('ii')!=-1?x.replace(/(ii)/,date['ii']):x.replace(/(i)/,date['i']);
+	if(x.indexOf('sss')!=-1){	x=x.replace(/(sss)/,date['sss']);	}
+	x=x.indexOf('ss')!=-1?x.replace(/(ss)/,date['ss']):x.replace(/(s)/,date['s']);
 	return x;
 }
 
@@ -38,4 +38,18 @@ function __getBackDateTime(f,format){
 	arr['i']?dt.setMinutes(dt.getMinutes()+parseInt(arr['i'])):null;
 	arr['s']?dt.setSeconds(dt.getSeconds()+parseInt(arr['s'])):null;
 	return __getCurrentDateTime(format,dt);
+}
+
+function __compareDates(date1,date2){
+	var x=date1.split('-'),d1,d2;
+	d1=new Date(x).getTime();
+	if(date2){
+		x=date2.split('-');
+		d2=new Date(x).getTime();
+	}
+	else{
+		x=new Date();
+		d2=new Date(x.getFullYear(),x.getMonth()+1,x.getDate()).getTime();
+	}
+	return d1==d2?0:((d1-d2)<0?-1:1);
 }
